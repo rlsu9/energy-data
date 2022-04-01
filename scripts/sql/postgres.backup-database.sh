5958bd868210c863d927cff27b0d60d449b2a58f
Yibo Guo <nil.yibo@gmail.com>
2022-04-01 16:18:06 -0700

    Added script to backup/restore postgres database.

---
+++ scripts/sql/postgres.backup-database.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo >&2 "Usage: $0 dbname dumpfile"
+    exit 1
+fi
+
+dbname="$1"
+dumpfile="$2"
+
+set -x
+pg_dump $dbname | gzip -c > "$dumpfile"
+
---
+++ scripts/sql/postgres.restore-database.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+    echo >&2 "Usage: $0 dbname dumpfile"
+    exit 1
+fi
+
+dbname="$1"
+dumpfile="$2"
+
+set -x
+cat "$dumpfile" | gunzip | psql --set ON_ERROR_STOP=on $dbname
+pg_dump $dbname | gzip -c > "$dumpfile"
+
