6a04b88ac97073d0e28d012886800e88f2c3a35b
Yibo Guo <nil.yibo@gmail.com>
2022-04-27 10:58:25 -0700

    Added script to add readonly user.

---
+++ database/user/create-user.sql
@@ -0,0 +1,1 @@
+CREATE ROLE xxx LOGIN PASSWORD 'yyy';
---
+++ database/user/grant-readonly-access.sql
@@ -0,0 +1,4 @@
+GRANT CONNECT ON DATABASE mydb TO xxx;
+-- This assumes you're actually connected to mydb..
+GRANT USAGE ON SCHEMA public TO xxx;
+GRANT SELECT ON mytable TO xxx;
