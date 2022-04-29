eed0bab374c029229e74d795773947a77f8ac2bc
Yibo Guo <nil.yibo@gmail.com>
2022-04-28 21:54:38 -0700

    Added basic electricityMap API.

---
+++ api/external/electricitymap/get-zones.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Source: https://static.electricitymap.org/api/docs/index.html#zones
+
+import requests
+
+list_url = 'https://api.electricitymap.org/v3/zones'
+rsp=requests.get(list_url)
+print(rsp.text)
