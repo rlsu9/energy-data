c352e988b6c7c837a530c8e9990a8255ef3bb797
Yibo Guo <nil.yibo@gmail.com>
2022-04-28 21:46:33 -0700

    Added argument to pull all balancing authorities.

--- api/external/watttime/ba-access.py
+++ api/external/watttime/ba-access.py
@@ -4,10 +4,15 @@
 
 import requests
 from util import get_watttime_token
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--all-regions', action='store_true', help='Get all regions')
+args = parser.parse_args()
 
 list_url = 'https://api2.watttime.org/v2/ba-access'
 headers = {'Authorization': 'Bearer {}'.format(get_watttime_token())}
-params = {'all': 'false'}
+params = {'all': str(args.all_regions).lower()}
 rsp=requests.get(list_url, headers=headers, params=params)
 print(rsp.text)
 
