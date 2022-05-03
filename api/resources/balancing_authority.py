231877331d30f2a97ea94670b5e26d73e4e54f38
Yibo Guo <nil.yibo@gmail.com>
2022-05-03 15:49:28 -0700

    Use WattTime API to look up balancing authority.

--- api/resources/balancing_authority.py
+++ api/resources/balancing_authority.py
@@ -4,6 +4,8 @@ from flask_restful import Resource
 from webargs import fields
 from webargs.flaskparser import use_kwargs
 
+from external.watttime.ba_from_loc import get_ba_from_loc
+
 balancing_authority_args = {
     'latitude': fields.Float(required=True, validate=lambda x: abs(x) <= 90.),
     'longitude': fields.Float(required=True, validate=lambda x: abs(x) <= 180.),
@@ -12,10 +14,9 @@ balancing_authority_args = {
 class BalancingAuthority(Resource):
     @use_kwargs(balancing_authority_args, location='query')
     def get(self, latitude, longitude):
-        # TODO: query this from WattTime API
-        balancing_authority_name = 'TBD'
+        (watttime_response, watttime_status_code) = get_ba_from_loc(latitude, longitude)
         return {
             'latitude': latitude,
             'longitude': longitude,
-            'balancing_authority': balancing_authority_name
-        }
+            'balancing_authority': watttime_response
+        }, watttime_status_code
