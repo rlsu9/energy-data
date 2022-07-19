1ca06084e2591b4b6afe3380f7fb0566e5d0bb51
Yibo Guo <nil.yibo@gmail.com>
2022-07-18 17:52:52 -0700

    Added cloud location validation.

--- api/resources/carbon_aware_scheduler.py
+++ api/resources/carbon_aware_scheduler.py
@@ -7,13 +7,13 @@ from flask_restful import Resource
 from webargs import fields
 from webargs.flaskparser import use_args, use_kwargs
 import marshmallow_dataclass
-from marshmallow import validate
+from marshmallow import validate, validates_schema, ValidationError
 
 from api.util import logger, PSqlExecuteException
 from api.resources.balancing_authority import convert_watttime_ba_abbrev_to_region, lookup_watttime_balancing_authority
-from api.resources.cloud_location import CloudLocationManager
+from api.resources.cloud_location import CloudLocationLookupException, CloudLocationManager
 
-g_cloud_locations = CloudLocationManager.get_all_cloud_locations()
+g_cloud_manager = CloudLocationManager()
 
 validate_timedelta_is_positive = lambda dt: dt.total_seconds() > 0
 metadata_timedelta = dict(
@@ -22,6 +22,12 @@ metadata_timedelta = dict(
     serialization_type = float
 )
 validate_number_is_nonnegative = validate.Range(min=0, min_inclusive=True)
+
+def field_default():
+    return field(metadata=dict())
+
+def field_with_validation(validation_function):
+    return field(metadata=dict(validate=validation_function))
 
 class ScheduleType(Enum):
     UNIFORM_RANDOM = "uniform-random"
@@ -34,19 +40,32 @@ class WorkloadSchedule:
 
 @dataclass
 class Dataset:
-    input_size_gb: float = field(metadata=dict(validate=validate_number_is_nonnegative))
-    output_size_gb: float = field(metadata=dict(validate=validate_number_is_nonnegative))
+    input_size_gb: float = field_with_validation(validate_number_is_nonnegative)
+    output_size_gb: float = field_with_validation(validate_number_is_nonnegative)
+
+@dataclass
+class CloudLocation:
+    cloud_provider: str = field_with_validation(validate.OneOf(g_cloud_manager.get_all_cloud_providers()))
+    region_code: str = field_default() #field_with_validation(validate.OneOf(g_cloud_manager.get_cloud_region_codes(cloud_provider)))
+
+    @validates_schema
+    def validate_schema(self, data, **kwargs):
+        # cloud_provider has been validated by its field-specific validation function
+        all_region_codes = g_cloud_manager.get_cloud_region_codes(data['cloud_provider'])
+        if data['region_code'] not in all_region_codes:
+            raise ValidationError({
+                'region_code': 'Must be one of: %s.' % ', '.join(all_region_codes)
+            })
 
 @dataclass
 class Workload:
+    preferred_cloud_location: CloudLocation
     runtime: timedelta = field(metadata=metadata_timedelta)
     schedule: WorkloadSchedule
     dataset: Dataset
 
 
 carbon_aware_scheduler_args = {
-    'latitude': fields.Float(required=True, validate=lambda x: abs(x) <= 90.),
-    'longitude': fields.Float(required=True, validate=lambda x: abs(x) <= 180.),
     'start': fields.DateTime(format="iso", required=True),
     'end': fields.DateTime(format="iso", required=True),
 }
@@ -54,16 +73,22 @@ carbon_aware_scheduler_args = {
 class CarbonAwareScheduler(Resource):
     @use_kwargs(carbon_aware_scheduler_args, location='query')
     @use_args(marshmallow_dataclass.class_schema(Workload)())
-    def get(self, args: Workload, latitude: float, longitude: float, start: datetime, end: datetime):
+    def get(self, args: Workload, start: datetime, end: datetime):
         workload = args
         orig_request = { 'request': {
-            'latitude': latitude,
-            'longitude': longitude,
             'start': start,
             'end': end,
             'workload': workload
         } }
-        logger.info("CarbonAwareScheduler.get(%f, %f, %s, %s, %s)" % (latitude, longitude, start, end, workload))
+        logger.info("CarbonAwareScheduler.get(%s, %s, %s)" % (start, end, workload))
+
+        try:
+            (latitude, longitude) = g_cloud_manager.get_gps_coordinate(args.preferred_cloud_location.cloud_provider,
+                                                                    args.preferred_cloud_location.region_code)
+        except CloudLocationLookupException as e:
+            return orig_request | {
+                'error': 'Requested cloud location not found: ' + str(e)
+            }, 404
 
         watttime_lookup_result, error_status_code = lookup_watttime_balancing_authority(latitude, longitude)
         if error_status_code:
--- api/resources/cloud_location.py
+++ api/resources/cloud_location.py
@@ -6,10 +6,10 @@ import os
 from pathlib import Path
 from typing import Tuple
 
-from api.util import loadYamlData
+from api.util import loadYamlData, logger
 
-class CloudProvider(Enum):
-    AWS = 'AWS'
+class CloudLocationLookupException(Exception):
+    pass
 
 @dataclass
 class CloudRegion:
@@ -20,23 +20,23 @@ class CloudRegion:
 
 @dataclass
 class PublicCloud:
-    provider: CloudProvider
+    provider: str
     regions: list[CloudRegion]
 
 class CloudLocationManager:
-    @classmethod
-    def get_all_cloud_locations(self) -> dict[str, list[PublicCloud]]:
+    all_public_clouds: dict[str, PublicCloud] = {}
+
+    def __init__(self) -> None:
         config_path = os.path.join(Path(__file__).parent.absolute(), 'cloud_location.yaml')
         yaml_data = loadYamlData(config_path)
         public_cloud_list_name = 'public_clouds'
         assert yaml_data is not None and public_cloud_list_name in yaml_data, \
             f'Failed to load {public_cloud_list_name}'
         l_raw_public_clouds = yaml_data[public_cloud_list_name]
-        l_public_clouds = []
         for raw_public_cloud in l_raw_public_clouds:
             cloud_provider = raw_public_cloud['provider']
             l_raw_cloud_regions = raw_public_cloud['regions']
-            l_cloud_regions = []
+            l_cloud_regions: list[CloudRegion] = []
             for raw_cloud_region in l_raw_cloud_regions:
                 region_code = raw_cloud_region['code']
                 region_name = raw_cloud_region['name']
@@ -46,5 +46,24 @@ class CloudLocationManager:
                 new_cloud_region = CloudRegion(cloud_provider, region_code, region_name, region_gps)
                 l_cloud_regions.append(new_cloud_region)
             new_public_cloud = PublicCloud(cloud_provider, l_cloud_regions)
-            l_public_clouds.append(new_public_cloud)
-        return l_public_clouds
+            self.all_public_clouds[cloud_provider] = new_public_cloud
+
+    def get_all_clouds_by_provider(self) -> dict[str, PublicCloud]:
+        return self.all_public_clouds
+
+    def get_all_cloud_providers(self) -> list[str]:
+        return sorted(self.all_public_clouds.keys())
+
+    def get_cloud_region_codes(self, cloud_provider: str) -> list[str]:
+        logger.debug('get_cloud_region_codes(%s)' % cloud_provider)
+        if cloud_provider not in self.all_public_clouds:
+            return []
+        return [region.code for region in self.all_public_clouds[cloud_provider].regions]
+
+    def get_gps_coordinate(self, cloud_provider: str, region_code: str) -> Tuple[float, float]:
+        if cloud_provider not in self.all_public_clouds:
+            raise CloudLocationLookupException('Unknown cloud provider "%s".' % cloud_provider)
+        for region in self.all_public_clouds[cloud_provider].regions:
+            if region.code == region_code:
+                return region.gps
+        raise CloudLocationLookupException('Unknown region "%s" for provider "%s".' % (region_code, cloud_provider))
