786393ce3db3b81fac6cfdfc7c359c4a64327ebf
Yibo Guo <nil.yibo@gmail.com>
2022-07-21 14:58:22 -0700

    Code refactoring.

--- api/resources/carbon_aware_scheduler.py
+++ api/resources/carbon_aware_scheduler.py
@@ -1,83 +1,17 @@
 #!/usr/bin/env python3
 
-from dataclasses import dataclass, field
-from enum import Enum
-from datetime import datetime, timedelta, timezone
-from typing import Optional
+from datetime import datetime
 from flask_restful import Resource
 from webargs import fields
 from webargs.flaskparser import use_args, use_kwargs
 import marshmallow_dataclass
-from marshmallow import validate, validates_schema, ValidationError
 
 from api.util import logger, PSqlExecuteException
 from api.resources.balancing_authority import convert_watttime_ba_abbrev_to_region, lookup_watttime_balancing_authority
 from api.resources.cloud_location import CloudLocationLookupException, CloudLocationManager
+from api.resources.models.workload import Workload
 
 g_cloud_manager = CloudLocationManager()
-
-validate_timedelta_is_positive = lambda dt: dt.total_seconds() > 0
-metadata_timedelta = dict(
-    validate = validate_timedelta_is_positive,
-    precision = 'seconds',
-    serialization_type = float
-)
-validate_number_is_nonnegative = validate.Range(min=0, min_inclusive=True)
-
-def field_default():
-    return field(metadata=dict())
-
-def field_with_validation(validation_function):
-    return field(metadata=dict(validate=validation_function))
-
-class ScheduleType(Enum):
-    UNIFORM_RANDOM = "uniform-random"
-    POISSON = "poisson"
-    ONETIME = "onetime"
-
-@dataclass
-class WorkloadSchedule:
-    type: ScheduleType = field(metadata=dict(by_value=True))
-    start_time: datetime = field_with_validation(validate.Range(min=datetime.now(timezone.utc)))
-    interval: Optional[timedelta] = field(metadata=metadata_timedelta, default=None)
-
-    @validates_schema
-    def validate_schema(self, data, **kwargs):
-        errors = dict()
-        if 'type' not in data:
-            return
-        if data['type'] is ScheduleType.ONETIME and 'interval' in data and data['interval']:
-            errors['interval'] = 'interval must be empty for one-time workload'
-        if data['type'] is not ScheduleType.ONETIME and ('interval' not in data or not data['interval']):
-            errors['interval'] = 'interval must be specified for recurring workload'
-        if errors:
-            raise ValidationError(errors)
-
-@dataclass
-class Dataset:
-    input_size_gb: float = field_with_validation(validate_number_is_nonnegative)
-    output_size_gb: float = field_with_validation(validate_number_is_nonnegative)
-
-@dataclass
-class CloudLocation:
-    cloud_provider: str = field_with_validation(validate.OneOf(g_cloud_manager.get_all_cloud_providers()))
-    region_code: str = field_default() #field_with_validation(validate.OneOf(g_cloud_manager.get_cloud_region_codes(cloud_provider)))
-
-    @validates_schema
-    def validate_schema(self, data, **kwargs):
-        # cloud_provider has been validated by its field-specific validation function
-        all_region_codes = g_cloud_manager.get_cloud_region_codes(data['cloud_provider'])
-        if data['region_code'] not in all_region_codes:
-            raise ValidationError({
-                'region_code': 'Must be one of: %s.' % ', '.join(all_region_codes)
-            })
-
-@dataclass
-class Workload:
-    preferred_cloud_location: CloudLocation
-    runtime: timedelta = field(metadata=metadata_timedelta)
-    schedule: WorkloadSchedule
-    dataset: Dataset
 
 
 carbon_aware_scheduler_args = {
