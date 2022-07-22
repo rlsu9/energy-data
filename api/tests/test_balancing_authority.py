762d32b186ce6e493e2c643c1fc4cdeb1ded0858
Yibo Guo <nil.yibo@gmail.com>
2022-07-21 20:10:39 -0700

    Minor test code refactoring.

--- api/tests/test_balancing_authority.py
+++ api/tests/test_balancing_authority.py
@@ -3,19 +3,19 @@
 import pytest
 from api.tests.util import logger, assert_response_ok
 
-def test_balancing_authority_list(client):
-    response = client.get('/balancing-authority/list')
-    assert_response_ok(response)
-
-    expected_regions_subset = {
-        'US-BPA',
-        'US-CAISO',
-    }
-    actual_regions: list[str] = response.json
-    logger.debug("Received %d regions." % len(actual_regions))
-    assert expected_regions_subset.issubset(actual_regions)
-
 class TestBalancingAuthority_Get:
+    def test_balancing_authority_list(self, client):
+        response = client.get('/balancing-authority/list')
+        assert_response_ok(response)
+
+        expected_regions_subset = {
+            'US-BPA',
+            'US-CAISO',
+        }
+        actual_regions: list[str] = response.json
+        logger.debug("Received %d regions." % len(actual_regions))
+        assert expected_regions_subset.issubset(actual_regions)
+    
     @pytest.mark.parametrize('coordinate, expected_region, location', [
         ((32.8801, -117.2340), 'US-CAISO', "UCSD"),
         ((43.0389, -87.9065), 'US-MISO', "Milwaukee, WI"),
