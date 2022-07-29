46823cd6882f0bbdd9856d222b516ca571795fd1
Yibo Guo <nil.yibo@gmail.com>
2022-07-29 15:34:58 -0700

    Re-formatted file to style guide.

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -10,10 +10,10 @@ import parsers.US_NEISO
 import parsers.US_BPA
 import parsers.US_NY
 import parsers.US_SPP
-import parsers.US_ERCOT
-import parsers.US_PREPA
+# import parsers.US_ERCOT
+# import parsers.US_PREPA
 import parsers.US_EIA
-from dateutil import tz, parser
+from dateutil import tz
 import arrow
 import psycopg2, psycopg2.extras
 import argparse
@@ -116,21 +116,25 @@ OVERRIDE_DATA_SOURCES = MAP_OVERRIDE_FETCHFNS.keys()
 parser = argparse.ArgumentParser()
 parser.add_argument('-B', '--backfill', action='store_true', help='Run backfill')
 parser.add_argument('-D', '--days-for-backfill', type=int, help='Number of days to run backfill for')
-parser.add_argument('--backfill-begin-date', type=lambda s: datetime.strptime(s, '%Y-%m-%d'), help='The begin date for backfill')
-parser.add_argument('--backfill-end-date', type=lambda s: datetime.strptime(s, '%Y-%m-%d'), help='The end date for backfill')
+parser.add_argument('--backfill-begin-date', type=lambda s: datetime.strptime(s, '%Y-%m-%d'),
+                    help='The begin date for backfill')
+parser.add_argument('--backfill-end-date', type=lambda s: datetime.strptime(s, '%Y-%m-%d'),
+                    help='The end date for backfill')
 parser.add_argument('-R', '--regions', nargs='+', choices=map_regions.keys(), help='Select a subset of regions')
 parser.add_argument('-N', '--dry-run', action='store_true', help='Only pull data but do not write to database')
 parser.add_argument('-F', '--force', action='store_true', help='Force pulling new data and ignore last updated')
 parser.add_argument('--override-data-source', choices=OVERRIDE_DATA_SOURCES, help='Override the crawler data source')
 args = parser.parse_args()
 
-def getdbconn(host='/var/run/postgresql/', database="electricity-data"):
+
+def get_db_connection(host='/var/run/postgresql/', database="electricity-data"):
     try:
         conn = psycopg2.connect(host=host, database=database, user="crawler_rw")
         return conn
     except Exception as e:
         print("Failed to connect to database.")
         raise e
+
 
 def get_last_updated(conn, region):
     with conn, conn.cursor() as cur:
@@ -142,15 +146,17 @@ def get_last_updated(conn, region):
             raise e
     return result[0] if result is not None else datetime.min
 
+
 def set_last_updated(conn, region, run_timestamp):
     with conn, conn.cursor() as cur:
         try:
             cur.execute("""INSERT INTO LastUpdated (Region, LastUpdated) VALUES (%s, %s)
                             ON CONFLICT (Region) DO UPDATE SET LastUpdated = EXCLUDED.LastUpdated""",
-                            [region, run_timestamp])
+                        [region, run_timestamp])
         except psycopg2.Error as e:
             print("Failed to execute set_last_updated query.")
             raise e
+
 
 def is_in_scheduled_downtime(region: str, e: Exception):
     if 'scheduledDowntime' not in map_regions[region]:
@@ -162,8 +168,9 @@ def is_in_scheduled_downtime(region: str, e: Exception):
         return True
     return False
 
+
 def fetch_new_data(region, target_datetime: datetime = None):
-    fetchFn = map_regions[region]['fetchFn']
+    fetch_fn = map_regions[region]['fetchFn']
     l_result = []
     if not args.backfill and map_regions[region]['fetchCurrentData']:
         target_datetime = None
@@ -176,9 +183,9 @@ def fetch_new_data(region, target_datetime: datetime = None):
             target_datetime = arrow.get(date.today(), map_regions[region]['timeZone'])
     print('Target datetime:', target_datetime)
     if args.override_data_source:
-        fetchFn = MAP_OVERRIDE_FETCHFNS[args.override_data_source]
+        fetch_fn = MAP_OVERRIDE_FETCHFNS[args.override_data_source]
     try:
-        l_data = fetchFn(zone_key=region, target_datetime=target_datetime)
+        l_data = fetch_fn(zone_key=region, target_datetime=target_datetime)
         if not map_regions[region]['fetchResultIsList'] and not args.override_data_source:
             l_data = [l_data]
     except Exception as e:
@@ -187,7 +194,7 @@ def fetch_new_data(region, target_datetime: datetime = None):
             raise e
         else:
             l_data = []
-    l_data.sort(key=lambda e: e['datetime'])
+    l_data.sort(key=lambda o: o['datetime'])
     for data in l_data:
         timestamp = arrow.get(data['datetime']).to(map_regions[region]['timeZone']).datetime
         print('time:', timestamp)
@@ -198,6 +205,7 @@ def fetch_new_data(region, target_datetime: datetime = None):
         l_result.append((timestamp, d_power_mw_by_category))
     return l_result
 
+
 def upload_new_data(conn, region, timestamp, d_power_mw_by_category):
     rows = []
     for category in d_power_mw_by_category:
@@ -206,25 +214,29 @@ def upload_new_data(conn, region, timestamp, d_power_mw_by_category):
         rows.append(row)
     with conn, conn.cursor() as cur:
         try:
-            psycopg2.extras.execute_values(cur,
+            psycopg2.extras.execute_values(
+                cur,
                 """INSERT INTO EnergyMixture (datetime, category, power_mw, region)
                     VALUES %s
                     ON CONFLICT ON CONSTRAINT energymixture_unique_datetime_category_region
                     DO UPDATE SET Power_MW = EXCLUDED.Power_MW
                         WHERE EnergyMixture.Power_MW = 'NaN'
                 """,
-                rows)
+                rows
+            )
         except psycopg2.Error as e:
             print("Failed to upload new data")
             raise e
     return len(rows)
 
-def fetchandupdate(conn, region, run_timestamp):
+
+def fetch_and_update(conn, region, run_timestamp):
     if args.backfill:
         l_result = []
         if args.days_for_backfill:
             for date_offset in range(args.days_for_backfill):
-                l_result += fetch_new_data(region, target_datetime=arrow.get().shift(days= -1 - date_offset))
+                target_date = arrow.get().shift(days=-1 - date_offset).datetime
+                l_result += fetch_new_data(region, target_datetime=target_date)
         elif args.backfill_begin_date and args.backfill_end_date:
             backfill_current_date = arrow.get(args.backfill_begin_date)
             backfill_end_date = arrow.get(args.backfill_end_date)
@@ -245,22 +257,25 @@ def fetchandupdate(conn, region, run_timestamp):
         if not args.backfill:
             set_last_updated(conn, region, run_timestamp)
 
+
 def should_run_now(conn, region, run_timestamp):
     last_updated = get_last_updated(conn, region)
     delta_since_last_update = run_timestamp - last_updated
     print("Last updated: %s (%.0f min ago)" % (str(last_updated), delta_since_last_update.total_seconds() / 60))
     return delta_since_last_update > map_regions[region]['updateFrequency']
 
+
 def crawl_region(conn, region):
     print(f'Region: {region}')
     run_timestamp = datetime.now()
     if args.backfill or args.force:
-        fetchandupdate(conn, region, run_timestamp)
+        fetch_and_update(conn, region, run_timestamp)
     else:
         if should_run_now(conn, region, run_timestamp):
-            fetchandupdate(conn, region, run_timestamp)
+            fetch_and_update(conn, region, run_timestamp)
 
-def crawlall():
+
+def crawl_all_regions():
     print("Electricity data crawler running at", str(datetime.now()))
     if args.dry_run:
         print('Dry run mode is on.')
@@ -271,13 +286,14 @@ def crawlall():
         elif args.backfill_begin_date and args.backfill_end_date:
             if args.backfill_begin_date > args.backfill_end_date:
                 raise ValueError("backfill-begin-date cannot be later than backfill-end-date")
-            print('Backfill date range: [%s, %s]' % (\
+            print('Backfill date range: [%s, %s]' % (
                 args.backfill_begin_date.strftime('%Y-%m-%d'),
                 args.backfill_end_date.strftime('%Y-%m-%d')
-                ))
+            ))
         else:
-            raise ValueError("Backfill mode is on, but neither days-for-backfill nor (backfill-begin-date and backfill-end-date) is specified.")
-    conn = getdbconn()
+            raise ValueError("Backfill mode is on, but neither days-for-backfill nor "
+                             "(backfill-begin-date and backfill-end-date) is specified.")
+    conn = get_db_connection()
     for region in map_regions:
         if args.regions and region not in args.regions:
             continue
@@ -285,11 +301,11 @@ def crawlall():
             crawl_region(conn, region)
         except Exception as e:
             print(datetime.now().isoformat(),
-                    f"Exception occurred while crawling region {region}:",
-                    e,
-                    file=sys.stderr)
+                  f"Exception occurred while crawling region {region}: {e}",
+                  file=sys.stderr)
             print(traceback.format_exc(), file=sys.stderr)
     conn.close()
 
+
 if __name__ == '__main__':
-    crawlall()
+    crawl_all_regions()
