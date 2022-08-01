0217e85e3e6ddeb5d0980a04b929300caefaac81
Yibo Guo <nil.yibo@gmail.com>
2022-08-01 11:04:17 -0700

    Added a debug mode to run script.

--- deploy/run-flask-app.sh
+++ deploy/run-flask-app.sh
@@ -1,12 +1,33 @@
 #!/bin/zsh
 
-cd "$(dirname "$0")/.."
+cd "$(dirname "$0")/.." || exit 1
+
+while [[ $# -gt 0 ]]; do
+case $1 in
+  -D|--debug)
+    DEBUG=true
+    shift
+    ;;
+  *)
+    shift
+    ;;
+esac
+done
 
 set -e
-source $HOME/anaconda3/bin/activate
+source "$HOME/anaconda3/bin/activate"
 conda activate flask
-gunicorn --workers=4 \
-    --log-level info \
+if [[ -n $DEBUG ]]; then
+  gunicorn --workers=1 \
+    --log-level=debug \
+     --bind=localhost:8082 \
+     -t=3600 \
+     --reload \
+    'api:create_app()'
+else
+  gunicorn --workers=4 \
+    --log-level=info \
     --access-logfile - \
     --access-logformat '%({X-Real-IP}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"' \
     'api:create_app()'
+fi
