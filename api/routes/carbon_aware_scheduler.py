51ad9b74e9428fc0de74939959b4a632129ab674
Yibo Guo <nil.yibo@gmail.com>
2023-04-28 11:56:11 -0700

    Consolidate ISO lookup in parallel processing.

--- api/routes/carbon_aware_scheduler.py
+++ api/routes/carbon_aware_scheduler.py
@@ -47,7 +47,6 @@ def get_candidate_regions(candidate_providers: list[str], candidate_locations: l
     except Exception as ex:
         raise ValueError(f'Failed to get candidate regions: {ex}') from ex
 
-
 def lookup_iso_region(gps: tuple[float, float]):
     try:
         (latitude, longitude) = gps
@@ -56,8 +55,59 @@ def lookup_iso_region(gps: tuple[float, float]):
     except Exception as ex:
         raise ValueError(f'Failed to lookup ISO region: {ex}') from ex
 
-def calculate_workload_scores(workload: Workload, iso: str, carbon_data_source: CarbonDataSource, use_prediction: bool) ->\
-        tuple[dict[OptimizationFactor, float], dict[str, Any]]:
+def task_lookup_iso(region: CloudRegion) -> tuple:
+    if region.iso:
+        return str(region), region.iso, None, None
+    try:
+        iso = lookup_iso_region(region.gps)
+        return str(region), iso, None, None
+    except Exception as ex:
+        return str(region), None, str(ex), traceback.format_exc()
+
+def init_preload_carbon_data(_workload: Workload,
+                                    _carbon_data_source: CarbonDataSource,
+                                    _use_prediction: bool):
+    global workload, carbon_data_source, use_prediction
+    workload = _workload
+    carbon_data_source = _carbon_data_source
+    use_prediction = _use_prediction
+
+def preload_carbon_data(workload: Workload, iso: str, carbon_data_source: CarbonDataSource, use_prediction: bool):
+    carbon_data_store = dict()
+    running_intervals = workload.get_running_intervals_in_24h()
+    for (start, end) in running_intervals:
+        max_delay = workload.schedule.max_delay
+        carbon_data_store[(iso, start, end)] = get_carbon_intensity_list(iso, start, end + max_delay,
+                                                        carbon_data_source, use_prediction)
+    return carbon_data_store
+
+def task_preload_carbon_data(iso: str) -> tuple:
+    global workload, carbon_data_source, use_prediction
+    try:
+        carbon_data = preload_carbon_data(workload, iso, carbon_data_source, use_prediction)
+        return iso, carbon_data, None, None
+    except Exception as ex:
+        return iso, None, str(ex), traceback.format_exc()
+
+def init_parallel_process_candidate(_workload: Workload,
+                                    _carbon_data_source: CarbonDataSource,
+                                    _use_prediction: bool,
+                                    _carbon_data_store: dict):
+    global workload, carbon_data_source, use_prediction, carbon_data_store
+    workload = _workload
+    carbon_data_source = _carbon_data_source
+    use_prediction = _use_prediction
+    carbon_data_store = _carbon_data_store
+
+def get_preloaded_carbon_data(iso: str, start: datetime, end: datetime) -> list[dict]:
+    global carbon_data_store
+    key = (iso, start, end)
+    if key in carbon_data_store:
+        return carbon_data_store[key]
+    else:
+        raise ValueError(f'No carbon data found for iso {iso} in time range ({start}, {end})')
+
+def calculate_workload_scores(workload: Workload, iso: str) -> tuple[dict[OptimizationFactor, float], dict[str, Any]]:
     d_scores = {}
     d_misc = {}
     for factor in OptimizationFactor:
@@ -77,8 +127,7 @@ def calculate_workload_scores(workload: Workload, iso: str, carbon_data_source: 
                 d_misc['migration_duration'] = []
                 # 24 hour / 5 min = 288 slots
                 for (start, end) in running_intervals:
-                    l_carbon_intensity = get_carbon_intensity_list(iso, start, end + max_delay,
-                                                                    carbon_data_source, use_prediction)
+                    l_carbon_intensity = get_preloaded_carbon_data(iso, start, end)
                     carbon_intensity_by_timestamp = convert_carbon_intensity_list_to_dict(l_carbon_intensity)
                     total_compute_carbon_emissions, optimal_delay_time = calculate_total_carbon_emissions(
                         start, end, DEFAULT_CPU_POWER_PER_CORE, carbon_intensity_by_timestamp, max_delay)
@@ -128,23 +177,12 @@ def calculate_workload_scores(workload: Workload, iso: str, carbon_data_source: 
         d_scores[factor] = score
     return d_scores, d_misc
 
-def init_parallel_process_candidate(_workload: Workload,
-                                    _carbon_data_source: CarbonDataSource,
-                                    _use_prediction: bool):
-    global workload, carbon_data_source, use_prediction
-    workload = _workload
-    carbon_data_source = _carbon_data_source
-    use_prediction = _use_prediction
-
-def process_candidate(region: CloudRegion) -> tuple:
+def task_process_candidate(region: CloudRegion) -> tuple:
     global workload, carbon_data_source, use_prediction
     region_name = str(region)
     iso = region.iso
     try:
-        if not iso:
-            iso = lookup_iso_region(region.gps)
-        scores, d_misc = calculate_workload_scores(workload, iso, carbon_data_source, use_prediction)
-        # region_name, iso, scores, d_misc, ex, stack_trace
+        scores, d_misc = calculate_workload_scores(workload, iso)
         return region_name, iso, scores, d_misc, None, None
     except Exception as ex:
         return region_name, iso, None, None, str(ex), traceback.format_exc()
@@ -162,36 +200,71 @@ class CarbonAwareScheduler(Resource):
             if workload.schedule.start_time < min_start_time:
                 workload.schedule.start_time = min_start_time
 
-        candidate_cloud_regions = get_candidate_regions(args.candidate_providers, args.candidate_locations)
-        d_cloud_region_to_iso = dict()
+        candidate_regions = get_candidate_regions(args.candidate_providers, args.candidate_locations)
+
+        d_region_isos = dict()
         d_region_scores = dict()
         d_region_warnings = dict()
         d_misc_details = dict()
-        with Pool(2,
-                  initializer=init_parallel_process_candidate,
+
+        with Pool(4) as pool:
+            result_iso = pool.map(task_lookup_iso, candidate_regions)
+        for i in range(len(candidate_regions)):
+            (region_name, iso, ex, stack_trace) = result_iso[i]
+            if iso:
+                d_region_isos[region_name] = iso
+                candidate_regions[i].iso = iso
+            else:
+                d_region_warnings[region_name] = ex
+                current_app.logger.error(f'ISO lookup failed for {region_name}: {ex}')
+                current_app.logger.error(stack_trace)
+
+        unique_isos = set(d_region_isos.values())
+        carbon_data = dict()
+        d_iso_errors = dict()
+        with Pool(4,
+                  initializer=init_preload_carbon_data,
                   initargs=(workload, args.carbon_data_source, args.use_prediction)
                   ) as pool:
-            result = pool.map(process_candidate, candidate_cloud_regions)
-            for (region_name, iso, scores, d_misc, ex, stack_trace) in result:
-                d_cloud_region_to_iso[region_name] = iso
-                if not ex:
-                    d_region_scores[region_name] = scores
-                    d_misc_details[region_name] = d_misc
+            result = pool.map(task_preload_carbon_data, unique_isos)
+        for (iso, partial_carbon_data, ex, stack_trace) in result:
+            if partial_carbon_data:
+                carbon_data |= partial_carbon_data
+            else:
+                d_iso_errors[iso] = ex
+                current_app.logger.error(f'Carbon data lookup failed for {iso}: {ex}')
+                current_app.logger.error(stack_trace)
+
+        with Pool(8,
+                  initializer=init_parallel_process_candidate,
+                  initargs=(workload, args.carbon_data_source, args.use_prediction, carbon_data)
+                  ) as pool:
+            result = pool.map(task_process_candidate, candidate_regions)
+        for (region_name, iso, scores, d_misc, ex, stack_trace) in result:
+            d_region_isos[region_name] = iso
+            if not ex:
+                d_region_scores[region_name] = scores
+                d_misc_details[region_name] = d_misc
+            else:
+                if d_iso_errors.get(iso, None):
+                    d_region_warnings[region_name] = d_iso_errors[iso]
                 else:
                     d_region_warnings[region_name] = str(ex)
                     current_app.logger.error(f'Exception when calculating score for region {region_name}: {ex}')
                     current_app.logger.error(stack_trace)
+
         best_region, d_weighted_scores = g_optimizer.compare_candidates(d_region_scores, True)
         if best_region is None:
             return orig_request | {
                 'error': 'No viable candidate',
+                'isos': d_region_isos,
                 'details': d_region_warnings
             }, 400
 
         return orig_request | {
             'original-region': str(args.original_location),
             'selected-region': best_region,
-            'isos': d_cloud_region_to_iso,
+            'isos': d_region_isos,
             'weighted-scores': d_weighted_scores,
             'raw-scores': d_region_scores,
             'warnings': d_region_warnings,
