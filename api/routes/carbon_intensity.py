ba8fc99f8264f361a21d4170d98bc0047ddb12a3
Yibo Guo <nil.yibo@gmail.com>
2023-05-01 16:18:52 -0700

    Use dataclass in carbon intensity API.

--- api/routes/carbon_intensity.py
+++ api/routes/carbon_intensity.py
@@ -2,37 +2,34 @@
 
 from datetime import datetime
 from flask_restful import Resource
-from webargs import fields
-from webargs.flaskparser import use_kwargs
+from webargs.flaskparser import use_args
 from flask import current_app
+import marshmallow_dataclass
 
 from api.helpers.carbon_intensity_c3lab import get_carbon_intensity_list
+from api.models.dataclass_extensions import *
 from api.routes.balancing_authority import convert_watttime_ba_abbrev_to_c3lab_region, \
     lookup_watttime_balancing_authority
 
-carbon_intensity_args = {
-    'latitude': fields.Float(required=True, validate=lambda x: abs(x) <= 90.),
-    'longitude': fields.Float(required=True, validate=lambda x: abs(x) <= 180.),
-    'start': fields.DateTime(format="iso", required=True),
-    'end': fields.DateTime(format="iso", required=True),
-}
+
+@marshmallow_dataclass.dataclass
+class CarbonIntensityRequest:
+    latitude: float = field_with_validation(lambda x: abs(x) <= 90.)
+    longitude: float = field_with_validation(lambda x: abs(x) <= 180.)
+    start: datetime = field_default()
+    end: datetime = field_default()
 
 
 class CarbonIntensity(Resource):
-    @use_kwargs(carbon_intensity_args, location='query')
-    def get(self, latitude: float, longitude: float, start: datetime, end: datetime):
-        orig_request = {'request': {
-            'latitude': latitude,
-            'longitude': longitude,
-            'start': start,
-            'end': end,
-        }}
-        current_app.logger.info("CarbonIntensity.get(%f, %f, %s, %s)" % (latitude, longitude, start, end))
+    @use_args(marshmallow_dataclass.class_schema(CarbonIntensityRequest)(), location='query')
+    def get(self, request: CarbonIntensityRequest):
+        orig_request = { 'request': request }
+        current_app.logger.info("CarbonIntensity.get(%s)" % request)
 
-        watttime_lookup_result = lookup_watttime_balancing_authority(latitude, longitude)
+        watttime_lookup_result = lookup_watttime_balancing_authority(request.latitude, request.longitude)
         iso = watttime_lookup_result['watttime_abbrev']
         region = convert_watttime_ba_abbrev_to_c3lab_region(iso)
-        l_carbon_intensity = get_carbon_intensity_list(iso, start, end)
+        l_carbon_intensity = get_carbon_intensity_list(iso, request.start, request.end)
 
         return orig_request | watttime_lookup_result | {
             'region': region,
