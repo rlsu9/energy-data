ea4f149f66af5fc378e776c2a406f2042b211daf
Yibo Guo <nil.yibo@gmail.com>
2023-05-22 11:45:45 -0700

    Added attribute category to fuel type table.

--- database/tables/table.carbon-intensity-by-fuel-type.sql
+++ database/tables/table.carbon-intensity-by-fuel-type.sql
@@ -1,5 +1,6 @@
 CREATE TABLE CarbonIntensityByFuelType (
     FuelType VARCHAR(32) NOT NULL,
     CarbonIntensity DOUBLE PRECISION NOT NULL,
+    Group VARCHAR(32) NOT NULL,
     UNIQUE (FuelType)
 )
