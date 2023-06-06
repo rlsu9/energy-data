2f523b364e4a824a07fa52cdef44d0d454731d73
Yibo Guo <nil.yibo@gmail.com>
2023-06-05 21:46:01 -0700

    Updated per-fuel-type carbon intensity constants.

---
+++ database/ad-hoc/20230605-carbon-intensity-constants.sql
@@ -0,0 +1,21 @@
+-- This script sets the carbon intensity constants.
+-- Source: electricityMap
+
+INSERT INTO CarbonIntensityByFuelType(FuelType, CarbonIntensity, "Group")
+VALUES
+    ('battery', 251, 'Renewable'),   -- only when discharging (positive amount of power)
+    ('biomass', 230, 'Renewable'),
+    ('coal', 820, 'NonRenewable'),
+    ('gas', 490, 'Renewable'),
+    ('geothermal', 38, , 'Renewable'),
+    ('hydro', 24, 'Renewable'),
+    ('import', 460, 'NonRenewable'),    -- average across the US for now.
+    ('nuclear', 12, 'Renewable'),
+    ('oil', 650, 'NonRenewable'),
+    ('other', 700, 'NonRenewable'),
+    ('solar', 45, 'Renewable'),
+    ('unknown', 700, 'NonRenewable'),
+    ('unknown-renewables', 230 , 'Renewable'),
+    ('wind', 11, 'Renewable'),
+    ('wind/solar', 28, 'Renewable')
+
