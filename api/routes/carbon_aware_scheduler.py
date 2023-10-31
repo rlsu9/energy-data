f1016edf8f226a5d96145855f82b4ad3a2f4b94a
Yibo Guo <nil.yibo@gmail.com>
2023-10-31 15:26:45 -0700

    Added units in scheduler response details.

--- api/routes/carbon_aware_scheduler.py
+++ api/routes/carbon_aware_scheduler.py
@@ -350,6 +350,8 @@ class CarbonAwareScheduler(Resource):
         for (region_name, iso, scores, d_misc, ex, stack_trace) in result:
             d_region_isos[region_name] = iso
             if not ex:
+                scores[OptimizationFactor.EnergyUsage + '-unit'] = 'kWh'
+                scores[OptimizationFactor.CarbonEmission + '-unit'] = 'gCO2'
                 d_region_scores[region_name] = scores
                 d_misc_details[region_name] = d_misc
             else:
